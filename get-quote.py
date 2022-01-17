ed19ed4d5664b8bd17a8046411b30fc7226a0132
Victor Passos <vicfilipe@gmail.com>
2022-01-16 23:58:57 -0300

    End of course

--- get-quote.py
+++ get-quote.py
@@ -1,15 +1,22 @@
 import random
 
+
 def primary():
     # print("Keep it logically awesome.")
 
+    f = open("quotes.txt", "a")
+    quote = input("Add quote:\n")
+    f.write(quote+'\n')
+    f.close()
     f = open("quotes.txt")
     quotes = f.readlines()
     f.close()
 
     last = len(quotes) - 1
     rnd = random.randint(0, last)
-    print(quotes[rnd])
+    rnd2 = random.randint(0, last)
+    print(quotes[rnd], end='')
+    print(quotes[rnd2], end='')
 
 
 if __name__ == "__main__":
